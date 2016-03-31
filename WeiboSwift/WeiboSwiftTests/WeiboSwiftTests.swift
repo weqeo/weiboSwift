523340ff3e7256276921ace6010083934dcadd7f
weqeo <weqeo1989@gmail.com>
2016-03-31 14:40:54 +0800

    初始化项目

---
+++ WeiboSwift/WeiboSwiftTests/WeiboSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeiboSwiftTests.swift
+//  WeiboSwiftTests
+//
+//  Created by 魏强 on 16/3/31.
+//  Copyright © 2016年 魏强. All rights reserved.
+//
+
+import XCTest
+@testable import WeiboSwift
+
+class WeiboSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
